0f644caa8793df8d03330416d17b92c6102361a4
Matthew Petro <99696010+mpetro-aa@users.noreply.github.com>
2023-06-12 03:29:05 +0000

    Changed tabs to spaces

--- java/com/aa/act/interview/org/Employee.java
+++ java/com/aa/act/interview/org/Employee.java
@@ -2,26 +2,26 @@ package com.aa.act.interview.org;
 
 public class Employee {
 
-	private int identifier;
-	private Name name;
+    private int identifier;
+    private Name name;
 
-	public Employee(int identifier, Name name) {
-		if(name == null)
-			throw new IllegalArgumentException("name cannot be null");
-		this.identifier = identifier;
-		this.name = name;
-	}
-	
-	public int getIdentifier() {
-		return identifier;
-	}
-	
-	public Name getName() {
-		return name;
-	}
+    public Employee(int identifier, Name name) {
+        if(name == null)
+            throw new IllegalArgumentException("name cannot be null");
+        this.identifier = identifier;
+        this.name = name;
+    }
+    
+    public int getIdentifier() {
+        return identifier;
+    }
+    
+    public Name getName() {
+        return name;
+    }
 
-	@Override
-	public String toString() {
-		return name.toString() + ": " + identifier;
-	}
+    @Override
+    public String toString() {
+        return name.toString() + ": " + identifier;
+    }
 }
--- java/com/aa/act/interview/org/Name.java
+++ java/com/aa/act/interview/org/Name.java
@@ -2,28 +2,28 @@ package com.aa.act.interview.org;
 
 public class Name {
 
-	private String first;
-	private String last;
-	
-	public Name(String first, String last) {
-		if(first == null)
-			throw new IllegalArgumentException("first name cannot be null");
-		if(last == null)
-			throw new IllegalArgumentException("last name cannot be null");
-		this.first = first;
-		this.last = last;
-	}
-	
-	public String getFirst() {
-		return first;
-	}
-	
-	public String getLast() {
-		return last;
-	}
+    private String first;
+    private String last;
+    
+    public Name(String first, String last) {
+        if(first == null)
+            throw new IllegalArgumentException("first name cannot be null");
+        if(last == null)
+            throw new IllegalArgumentException("last name cannot be null");
+        this.first = first;
+        this.last = last;
+    }
+    
+    public String getFirst() {
+        return first;
+    }
+    
+    public String getLast() {
+        return last;
+    }
 
-	@Override
-	public String toString() {
-		return first + " " + last;
-	}
+    @Override
+    public String toString() {
+        return first + " " + last;
+    }
 }
